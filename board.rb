8115bcba9de7595896087bc5d1b6366d908fd93b
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-06-07 18:27:32 -0700

    Made more methods private

--- board.rb
+++ board.rb
@@ -20,32 +20,12 @@ class Board
     display_path(path)
   end
 
-  def add_knight(square = [0, 0])
-    return if @graph.explored?(square)
-
-    @graph.explored(square)
-    moves = valid_moves(Knight.moves(square))
-    moves.each do |move|
-      @graph.add_vertex(move)
-      @graph.add_edge(square, move)
-      add_knight(move)
-    end
-  end
-
   def display_edges
     @graph.display_adj_list
   end
 
   def display_vertices
     @graph.display_vertex_list
-  end
-
-  def convert_from_chess(notation)
-    [notation[0].ord - 97, MAX + 1 - notation[1].to_i]
-  end
-
-  def convert_to_chess(coordinates)
-    (coordinates[0] + 97).chr + (MAX + 1 - coordinates[1]).to_s
   end
 
   private
@@ -64,5 +44,25 @@ class Board
       print ' --> ' unless square == path.last
     end
     puts
+  end
+
+  def add_knight(square = [0, 0])
+    return if @graph.explored?(square)
+
+    @graph.explored(square)
+    moves = valid_moves(Knight.moves(square))
+    moves.each do |move|
+      @graph.add_vertex(move)
+      @graph.add_edge(square, move)
+      add_knight(move)
+    end
+  end
+
+  def convert_from_chess(notation)
+    [notation[0].ord - 97, MAX + 1 - notation[1].to_i]
+  end
+
+  def convert_to_chess(coordinates)
+    (coordinates[0] + 97).chr + (MAX + 1 - coordinates[1]).to_s
   end
 end
